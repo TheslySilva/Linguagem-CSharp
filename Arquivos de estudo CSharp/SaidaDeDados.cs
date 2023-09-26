e81c2089b676e58b65e23db45b2e671c6c03afa5
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-26 14:10:03 -0300

    update Arquivos de estudo CSharp/SaidaDeDados.cs

---
+++ Arquivos de estudo CSharp/SaidaDeDados.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace MeuProjeto{
+
+	class Program{
+
+		static void Main(String[] args) {
+
+			//Variaveis
+
+			int idade = 21;
+			string nome = "Thesly Silva";
+			char sexo = 'M';
+			double salario = 1289.50;
+
+			//Saida de dados
+
+			Console.Write("Nome = " + nome);
+			Console.Write("\nIdade = " + idade);
+			Console.Write("\nSexo = " + sexo);
+			Console.Write("\nSalario = " + salario);
+
+		}
+	}
+}
