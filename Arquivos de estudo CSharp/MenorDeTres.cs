1d93c982ce8ab21fac59741df8bf916b971b41b0
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-27 10:57:51 -0300

    update Arquivos de estudo CSharp/MenorDeTres.cs

--- Arquivos de estudo CSharp/MenorDeTres.cs
+++ Arquivos de estudo CSharp/MenorDeTres.cs
@@ -4,19 +4,19 @@ namespace HelloWold;
 
 public static class Program {
 	public static void Main() {
-
+//Variaveis
 		int n1,
 		n2,
 		n3,
 		resultado;
-
+//Entrada
 		Console.Write("Digite um numero : ");
 		n1 = int.Parse(Console.ReadLine());
 		Console.Write("Digite outro numero : ");
 		n2 = int.Parse(Console.ReadLine());
 		Console.Write("Digite outro numero : ");
 		n3 = int.Parse(Console.ReadLine());
-
+//Condicoes
 		if (n1 < n2 && n1 < n3) {
 
 			resultado = n1;
@@ -27,7 +27,7 @@ public static class Program {
 
 			resultado = n3;
 		}
-
+//Saida
 		Console.WriteLine("\nO menor numero é : "+ resultado);
 	}
 }
