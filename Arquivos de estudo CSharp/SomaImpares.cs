0817856fc04ac47caf191acd38f445d6ddd70a7e
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-27 11:24:13 -0300

    update Arquivos de estudo CSharp/SomaImpares.cs

---
+++ Arquivos de estudo CSharp/SomaImpares.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Globalization;
+
+
+namespace HelloWold;
+
+public static class Program
+{
+       public static void Main()
+       {
+              int i, n1, n3, n2, soma;
+
+
+
+              Console.Write("Digite um numero : ");
+              n1 = int.Parse(Console.ReadLine());
+
+              Console.Write("Digite um numero : ");
+              n2 = int.Parse(Console.ReadLine());
+
+              if (n1 > n2)
+              {
+
+                     n3 = n1;
+                     n1 = n2;
+                     n2 = n3;
+              }
+
+              soma = 0;
+
+              for (i = n1 + 1; i <= n2 - 1; i++)
+              {
+                     if (i%2 != 0)
+                     {
+                            soma = soma + i;
+                     }
+
+              }
+              Console.WriteLine("A SOMA: " + soma);
+
+
+       }
+}
